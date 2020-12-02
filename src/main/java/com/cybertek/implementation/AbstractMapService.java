03bab17dc8450b5c579718e530fff257eb090950
cybertekschool <github@cybertekschool.com>
2020-12-01 19:26:16 -0500

    Ticketing Project MVC

--- src/main/java/com/cybertek/implementation/AbstractMapService.java
+++ src/main/java/com/cybertek/implementation/AbstractMapService.java
@@ -32,7 +32,6 @@ public abstract class AbstractMapService<T,ID> {
     }
 
     void update(ID id,T object){
-        map.entrySet().removeIf(entry -> entry.getValue().equals(object));
         map.put(id,object);
     }
 
