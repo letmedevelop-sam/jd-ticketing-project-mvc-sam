d4bc97b7826643c1cc9417c7fe3ffd840ea23aad
SamKar <letmedevelopsam@gmail.com>
2021-05-12 15:15:48 +0300

    Services created

--- src/main/java/com/cybertek/service/RoleService.java
+++ src/main/java/com/cybertek/service/RoleService.java
@@ -7,6 +7,21 @@ import java.util.List;
 
 public interface RoleService extends CrudService<RoleDTO,Long> {
 
+/*  We will do them all
+    RoleDTO save(UserDTO);
+    RoleDTO finByID(String username);
+    List<RoleDTO> findAll();
+    void delete(RoleDTO user);
+    void deleteByID(Long id);
+    */
 
+    /*
+    Generic has 2 parameters
+    T save(T role);
+    T finByID(ID id);
+    List<T> findAll();
+    void delete(T role);
+    void deleteByID(Long id);
+     */
 
 }
